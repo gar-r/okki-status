e95b7363f4bf14806d4348b23c04f59a32430c75
Richard Garai <dargzero@gmail.com>
2019-06-06 15:30:50 +0000

    battery tests

--- modules/battery_test.go
+++ modules/battery_test.go
@@ -7,7 +7,7 @@ func TestBattery_Status(t *testing.T) {
 	b := &Battery{
 		Battery:  "BAT0",
 		Charging: "[C]", Discharging: "[D]",
-		Full: "[F]", High: "[H]", Normal: "N", Low: "[L]", Critical: "[C]", Unknown: "[?]",
+		Full: "[F]", High: "[H]", Normal: "[N]", Low: "[L]", Critical: "[C]", Unknown: "[?]",
 	}
 
 	var status, level, capacity string
@@ -25,17 +25,29 @@ func TestBattery_Status(t *testing.T) {
 		}
 	}
 
-	t.Run("test error", func(t *testing.T) {
-		status = "error"
-		level = "error"
-		capacity = "error"
+	tests := []struct {
+		status, level, capacity string
+		expected                string
+	}{
+		{"error", "error", "error", "[?] error%"},
+		{"Charging", "Full", "99", "[C] [F] 99%"},
+		{"Discharging", "High", "85", "[D] [H] 85%"},
+		{"Something", "Normal", "50", "[N] 50%"},
+		{"Something", "Low", "50", "[L] 50%"},
+		{"Something", "Critical", "50", "[C] 50%"},
+	}
 
-		expected := "[?] error%"
-		actual := b.Status()
+	for _, test := range tests {
+		t.Run("test battery status", func(t *testing.T) {
+			status = test.status
+			level = test.level
+			capacity = test.capacity
 
-		if expected != actual {
-			t.Errorf("expected '%s', got '%s'", expected, actual)
-		}
-	})
+			actual := b.Status()
 
+			if test.expected != actual {
+				t.Errorf("expected '%s', got '%s'", test.expected, actual)
+			}
+		})
+	}
 }
