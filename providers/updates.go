0302db04d2e74c2de7b089c5199e671a3a9b00aa
Richard Garai <dargzero@gmail.com>
2020-05-01 19:41:18 +0200

    simpler package count

--- providers/updates.go
+++ providers/updates.go
@@ -2,7 +2,6 @@ package providers
 
 import (
 	"os/exec"
-	"strconv"
 	"strings"
 )
 
@@ -12,19 +11,13 @@ type Updates struct {
 
 // GetStatus returns the number of updates as string
 func (u *Updates) GetStatus() string {
-	out, err := exec.Command("yay", "-Qu").Output()
+	out, err := exec.Command("yay", "-Pn").Output()
 	if err != nil {
 		return "?"
 	}
-	count := countUpdates(string(out))
-	return strconv.Itoa(count)
-}
-
-func countUpdates(s string) (count int) {
-	for _, line := range strings.Split(s, "\n") {
-		if len(strings.TrimSpace(line)) > 0 {
-			count++
-		}
+	lines := strings.Split(string(out), "\n")
+	if len(lines) < 1 {
+		return "?"
 	}
-	return
+	return lines[0]
 }
