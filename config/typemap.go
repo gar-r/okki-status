7dbc343e9642576ffa4daeb63d3097126c935d42
Richard Garai <garric@okki.hu>
2023-09-20 22:10:37 +0200

    brightness provider

--- config/typemap.go
+++ config/typemap.go
@@ -6,9 +6,10 @@ import (
 )
 
 var TypeMap = map[string]reflect.Type{
-	"clock":   reflect.TypeOf(provider.Clock{}),
-	"battery": reflect.TypeOf(provider.Battery{}),
-	"layout":  reflect.TypeOf(provider.Layout{}),
-	"volume":  reflect.TypeOf(provider.Volume{}),
-	"memory":  reflect.TypeOf(provider.Memory{}),
+	"clock":      reflect.TypeOf(provider.Clock{}),
+	"battery":    reflect.TypeOf(provider.Battery{}),
+	"layout":     reflect.TypeOf(provider.Layout{}),
+	"volume":     reflect.TypeOf(provider.Volume{}),
+	"memory":     reflect.TypeOf(provider.Memory{}),
+	"brightness": reflect.TypeOf(provider.Brightness{}),
 }
